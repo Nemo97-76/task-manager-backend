a071b45723d7f4b26321d982fb4a6bf45222c9c9
tasneem youssef <tasneemyoussef61@gmail.com>
2025-04-01 22:11:18 +0200

    add cors to index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cros=require('cors');
 const mongoose = require('mongoose');
 const authRoutes = require("./routes/auth")
 const taskRoutes = require("./routes/tasks")
+const cors= require("cors");
 require('dotenv').config();
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(cros());
 app.use(express.json());
 
 //connect DB
-mongoose.connect("mongodb://localhost:27017/task-manager").then(() => {
+mongoose.connect("mongodb+srv://tasneemyoussef61:Hg2YRJFj8CWQTIn2@taskmanger.dlfaors.mongodb.net/?retryWrites=true&w=majority&appName=TaskManger").then(() => {
     console.log('DB connected');
 }).catch((error) => {
     console.log("mongoDB connection error", error);
@@ -22,7 +23,9 @@ mongoose.connect("mongodb://localhost:27017/task-manager").then(() => {
 //Routes
 app.use('/auth', authRoutes);//auth routes
 app.use('/api/tasks', taskRoutes);//task routes
-
+app.use(cors({
+    origin:"http://localhost:5173"
+}))
 //root route for testing
 app.get('/', (req, res) => {
     res.send("task manager API is runnging");
